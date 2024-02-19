c0b9a8a59436c5f505c34f501348cc6098c8dec0
Nathan <nm7090@rit.edu>
2024-02-19 11:10:30 -0500

    initial commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+
+public class helloworld {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+
+            System.out.println("Testing again");
+        }
+}
